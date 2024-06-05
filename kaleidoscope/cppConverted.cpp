4509cbf2e10884aa233819d0e8134f03f979fe74
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-06-05 14:19:15 -0700

    Create cppConverted.cpp

---
+++ kaleidoscope/cppConverted.cpp
@@ -0,0 +1,7 @@
+auto add(int a, int b) { return (a + b); }
+auto main() {
+    int x = 10;
+    int y = 20;
+    int result = add(x, y);
+    if (result > 10) { return result; } else { return 0; }
+}
